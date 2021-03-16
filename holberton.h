f5a76f2999996a8e7cfc8fcaa23523077a259f1a
root <vincentjayden49@gmail.com>
2021-03-16 17:56:59 +0000

    update holberton

--- holberton.h
+++ holberton.h
@@ -1,8 +1,22 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
+
+/**
+ *@Cspecs: for test characters.
+ * Return: int, number of characters printed, 
+ */
+
+typedef struct Cspecs
+{
+char cs;
+void (*f)();
+} cs_t;
 
 int _printf(const char *format, ...);
-
+void _putchar_c(char c);
+void _putchar(va_list a);
+void print_str(va_list a);
 char *inttobinary(va_list list);
 
 #endif
